04c86e4ab4261a93f678d86b8f565c291f712f81
Matheus-Borges <84951351+Matheus-Borges-Never@users.noreply.github.com>
2022-06-02 16:34:31 -0300

    First commit

---
+++ cadastro/cadastro/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace cadastro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtgBusca = new System.Windows.Forms.DataGridView();
+            this.lblId = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtProduto = new System.Windows.Forms.TextBox();
+            this.txtEstoque = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.mtbData = new System.Windows.Forms.MaskedTextBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtEntrada = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtValor2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ccbEntrada = new System.Windows.Forms.RadioButton();
+            this.ccbSaida = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgBusca)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(341, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(354, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastro de Entrada";
+            // 
+            // dtgBusca
+            // 
+            this.dtgBusca.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dtgBusca.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dtgBusca.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgBusca.Location = new System.Drawing.Point(34, 184);
+            this.dtgBusca.Name = "dtgBusca";
+            this.dtgBusca.Size = new System.Drawing.Size(507, 355);
+            this.dtgBusca.TabIndex = 1;
+            this.dtgBusca.Click += new System.EventHandler(this.dtgBusca_Click);
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblId.Location = new System.Drawing.Point(841, 206);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(25, 25);
+            this.lblId.TabIndex = 2;
+            this.lblId.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Produto:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(662, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "ID:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(281, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Estoque:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(547, 124);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Valor:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(664, 279);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Estoque:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(664, 351);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(160, 20);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Data da Alteração:";
+            // 
+            // txtProduto
+            // 
+            this.txtProduto.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtProduto.Location = new System.Drawing.Point(113, 122);
+            this.txtProduto.Name = "txtProduto";
+            this.txtProduto.Size = new System.Drawing.Size(162, 20);
+            this.txtProduto.TabIndex = 10;
+            this.txtProduto.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtProduto.TextChanged += new System.EventHandler(this.txtProduto_TextChanged);
+            // 
+            // txtEstoque
+            // 
+            this.txtEstoque.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEstoque.Location = new System.Drawing.Point(368, 124);
+            this.txtEstoque.Name = "txtEstoque";
+            this.txtEstoque.Size = new System.Drawing.Size(162, 20);
+            this.txtEstoque.TabIndex = 11;
+            this.txtEstoque.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtEstoque.TextChanged += new System.EventHandler(this.txtEstoque_TextChanged);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(609, 124);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(162, 20);
+            this.txtValor.TabIndex = 12;
+            this.txtValor.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtValor.TextChanged += new System.EventHandler(this.txtValor_TextChanged);
+            // 
+            // mtbData
+            // 
+            this.mtbData.CutCopyMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+            this.mtbData.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbData.Location = new System.Drawing.Point(848, 351);
+            this.mtbData.Name = "mtbData";
+            this.mtbData.Size = new System.Drawing.Size(162, 20);
+            this.mtbData.TabIndex = 15;
+            this.mtbData.Text = "00/00/0000";
+            this.mtbData.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.BackColor = System.Drawing.Color.Green;
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.ForeColor = System.Drawing.Color.Transparent;
+            this.btnInserir.Location = new System.Drawing.Point(864, 110);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(125, 42);
+            this.btnInserir.TabIndex = 16;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = false;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.BackColor = System.Drawing.Color.Teal;
+            this.btnAtualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAtualizar.ForeColor = System.Drawing.Color.Transparent;
+            this.btnAtualizar.Location = new System.Drawing.Point(767, 408);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(137, 42);
+            this.btnAtualizar.TabIndex = 17;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = false;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(578, 493);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 46);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEntrada.Location = new System.Drawing.Point(848, 279);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(162, 20);
+            this.txtEntrada.TabIndex = 19;
+            this.txtEntrada.Text = "0";
+            this.txtEntrada.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(664, 319);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Valor:";
+            // 
+            // txtValor2
+            // 
+            this.txtValor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor2.Location = new System.Drawing.Point(848, 319);
+            this.txtValor2.Name = "txtValor2";
+            this.txtValor2.Size = new System.Drawing.Size(162, 20);
+            this.txtValor2.TabIndex = 22;
+            this.txtValor2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(664, 244);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 20);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Tipo:";
+            // 
+            // ccbEntrada
+            // 
+            this.ccbEntrada.AutoSize = true;
+            this.ccbEntrada.Location = new System.Drawing.Point(848, 247);
+            this.ccbEntrada.Name = "ccbEntrada";
+            this.ccbEntrada.Size = new System.Drawing.Size(62, 17);
+            this.ccbEntrada.TabIndex = 24;
+            this.ccbEntrada.TabStop = true;
+            this.ccbEntrada.Text = "Entrada";
+            this.ccbEntrada.UseVisualStyleBackColor = true;
+            // 
+            // ccbSaida
+            // 
+            this.ccbSaida.AutoSize = true;
+            this.ccbSaida.Location = new System.Drawing.Point(930, 246);
+            this.ccbSaida.Name = "ccbSaida";
+            this.ccbSaida.Size = new System.Drawing.Size(52, 17);
+            this.ccbSaida.TabIndex = 25;
+            this.ccbSaida.TabStop = true;
+            this.ccbSaida.Text = "Saida";
+            this.ccbSaida.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1053, 585);
+            this.Controls.Add(this.ccbSaida);
+            this.Controls.Add(this.ccbEntrada);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtValor2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.mtbData);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.txtEstoque);
+            this.Controls.Add(this.txtProduto);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.dtgBusca);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Saida";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgBusca)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dtgBusca;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtProduto;
+        private System.Windows.Forms.TextBox txtEstoque;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.MaskedTextBox mtbData;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox txtEntrada;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtValor2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RadioButton ccbEntrada;
+        private System.Windows.Forms.RadioButton ccbSaida;
+    }
+}
+
---
+++ cadastro/cadastro/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace cadastro
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEstoque_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtValor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            if (con.State == ConnectionState.Open)
+            {
+                SqlCommand cm;
+                string sql = "";
+                sql = "insert into cadastro_matheus (produto,estoque,vlr_unit) values (@PRODUTO,@ESTOQUE,@VLR_UNIT)";
+                cm = new SqlCommand(sql, con);
+
+                cm.Parameters.Add("@PRODUTO", SqlDbType.VarChar).Value = txtProduto.Text;
+                cm.Parameters.Add("@ESTOQUE", SqlDbType.VarChar).Value = txtEstoque.Text;
+                cm.Parameters.Add("@VLR_UNIT", SqlDbType.Decimal).Value = txtValor.Text;
+
+                int ret = cm.ExecuteNonQuery();
+                if (ret > 0)
+                {
+                    MessageBox.Show("O Produto foi inserido com sucesso!");
+
+                }
+            }
+            mostrar();
+
+        }
+
+        private void txtProduto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            if (con.State == ConnectionState.Open)
+            {
+                if (ccbEntrada.Checked == true)
+                {
+
+                    SqlCommand cm;
+                    string sql = "";
+                    string estoque = "";
+                    estoque = " update cadastro_matheus set ESTOQUE = ESTOQUE + @QUANTIDADE where id =@ID";
+                    sql = "update cadastro_matheus set VLR_UNIT=@VLR_UNIT, DATA_ALT=@DATA_ALT, TIPO= @ENTRADA where id=@ID";
+                    cm = new SqlCommand(sql + estoque, con);
+
+                    cm.Parameters.Add("@QUANTIDADE", SqlDbType.Int).Value = txtEntrada.Text;
+                    cm.Parameters.Add("@VLR_UNIT", SqlDbType.Decimal).Value = txtValor2.Text;
+                    cm.Parameters.Add("@DATA_ALT", SqlDbType.Date).Value = mtbData.Text;
+                    cm.Parameters.Add("@ID", SqlDbType.BigInt).Value = lblId.Text;
+                    cm.Parameters.Add("@ENTRADA", SqlDbType.VarChar).Value = "Entrada";
+
+                    int ret = cm.ExecuteNonQuery();
+                    if (ret > 0)
+                    {
+                        MessageBox.Show("O Produto foi alterado com sucesso!");
+                    }
+                }
+                else if (ccbSaida.Checked == true)
+                {
+
+                    SqlCommand cm;
+                    string sql = "";
+                    string estoque = "";
+                    estoque = " update cadastro_matheus set ESTOQUE = ESTOQUE - @QUANTIDADE where id =@ID";
+                    sql = "update cadastro_matheus set VLR_UNIT=@VLR_UNIT, DATA_ALT=@DATA_ALT, TIPO= @SAIDA where id=@ID";
+                    cm = new SqlCommand(sql + estoque, con);
+
+                    cm.Parameters.Add("@QUANTIDADE", SqlDbType.Int).Value = txtEntrada.Text;
+                    cm.Parameters.Add("@VLR_UNIT", SqlDbType.Decimal).Value = txtValor2.Text;
+                    cm.Parameters.Add("@DATA_ALT", SqlDbType.Date).Value = mtbData.Text;
+                    cm.Parameters.Add("@ID", SqlDbType.BigInt).Value = lblId.Text;
+                    cm.Parameters.Add("@SAIDA", SqlDbType.VarChar).Value = "Saida";
+
+                    int ret = cm.ExecuteNonQuery();
+                    if (ret > 0)
+                    {
+                        MessageBox.Show("O Produto foi alterado com sucesso!");
+                    }
+                }
+
+                mostrar();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string str = "";
+            str = "Data source = 10.64.45.32, 1433; Initial Catalog = TI41; User Id = aluno; Password = 123456";
+            con = new SqlConnection(str);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mostrar();
+        }
+
+        private void mostrar()
+        {
+            SqlCommand cm;
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable tb = new DataTable();
+            string sql = "select * from cadastro_matheus";
+            cm = new SqlCommand(sql, con);
+            da.SelectCommand = cm;
+            da.Fill(tb);
+            dtgBusca.DataSource = null;
+            dtgBusca.DataSource = tb;
+
+        }
+
+        private void dtgBusca_Click(object sender, EventArgs e)
+        {
+            if (dtgBusca.Rows.Count > 0)
+            {
+                lblId.Text = dtgBusca.CurrentRow.Cells[0].Value.ToString();
+                txtValor2.Text = dtgBusca.CurrentRow.Cells[3].Value.ToString();
+                mtbData.Text = dtgBusca.CurrentRow.Cells[4].Value.ToString();
+            }
+
+        }
+    }
+}
